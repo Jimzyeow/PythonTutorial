925fa9b93f49343badf17b2fafb797677cbfcc3a
Jimzyeow <zeanwee321@gmail.com>
2021-01-28 21:49:54 +0800

    day 4

---
+++ day 4 Randomisation and Python Lists/random.py
@@ -0,0 +1,11 @@
+import random
+
+#import another module (class)
+import my_module
+
+#randint(a,b) will print random number: a <= number <= b
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+#call the module variable
+print(my_module.pi)
